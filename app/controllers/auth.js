42c81c8d0718548272e8f5a661e87137b71033de
Imma-Olly <oluomajoan@gmail.com>
2020-08-04 10:23:29 -0400

    Created find or create function

--- app/controllers/auth.js
+++ app/controllers/auth.js
@@ -1,10 +1,10 @@
 const express = require("express");
+
 const models = require("../models");
 const config = require("../../config");
 const GitHub = require("../services/github");
-const controllers = require("../controllers");
-const router = express.Router();
 
+const router = express.Router();
 
 router.get("/logout", function(req, res) {
   req.session.destroy();
@@ -25,13 +25,11 @@ router.get('/callback/github', async (req, res) => {
   const github = new GitHub({ clientId: config.githubClientId, clientSecret: config.githubClientSecret });
   const accessToken = await github.getToken(req.query.code);
 
-  console.log("Access token: " + access_token);
-
   if (!access_token) {
     return res.render("404");
   }
 
- const user = await models.userSchema.statics.findOneOrCreate(access_token);
+  const user = userSchema.statics.findUser(access_token);
 
   req.session.access_token = access_token;
   req.session.user = user;
